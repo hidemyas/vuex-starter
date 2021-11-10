105d406dc5f2a9b6cb428d5ce7c8329cd98fde71
HidemYas <61700138+hidemyas@users.noreply.github.com>
2021-11-10 20:53:45 +0300

    comit one
    comit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+import contact from "./modules/contact";
+import taskmanager from "./modules/taskmanager";
+
+const store = createStore({
+  state: {
+    mainName: "www.yazilimmethod.com"
+  },
+  mutations: {},
+  modules: {
+    musteri: contact,
+    taskmanager
+  }
+});
+
+export default store;
